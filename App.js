b3eada5c423ad579d4e8179e4d3ec89a1849ee57
Caden1021 <71086321+Caden1021@users.noreply.github.com>
2022-03-08 15:55:19 -0500

    Updated App.js Imports

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import Login from "./Login";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { } from 'firebase/database';
-import { } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
