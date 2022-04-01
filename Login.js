acf35184084740a7d2e8526a415ea20f376fd785
jseiden12 <jseidenberg@albany.edu>
2022-03-31 20:04:40 -0400

    Reworked login screen and firebase config

---
+++ Login.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import {
+    View, 
+    Text,
+    TextInput,
+    Button,
+    Pressable,
+    Alert,
+    StyleSheet,
+} from 'react-native';
+
+function Login() {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={{fontSize: 25}}>Login</Text>
+            </View>
+            <Text style={{marginTop: 55}}>Email</Text>
+            <View>
+                <TextInput placeholder='Email' style={styles.input}/>
+            </View>
+            <Text style={{marginTop: 15}}>Password</Text>
+            <View>
+                <TextInput placeholder='Password' secureTextEntry={true} style={styles.input}/>
+            </View>
+            <Pressable style={styles.button} onPress={() => Alert.alert('Login Button Pressed')}>
+                <Text>Log in</Text>
+            </Pressable>
+            <Pressable style={styles.link} onPress={() => Alert.alert('Reset Password')}>
+                <Text>Forgot Password?</Text>
+            </Pressable>
+            <Pressable style={styles.link} onPress={() => Alert.alert('Sign Up')}>
+                <Text>Sign Up</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+export default Login;
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#cd9bf1'
+    },
+    input: {
+        backgroundColor: '#dac2f0',
+        height: 30,
+        width: 200,
+        marginTop: 10
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 10,
+        backgroundColor: '#bf94e4',
+        marginTop: 15
+    },
+    link: {
+        marginTop: 15,
+
+    },
+});
